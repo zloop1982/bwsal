c46baa6e1815e3c330256de92c5a7746f6b7a127
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2009-08-15 20:50:18 +0000

    SupplyManager now spends resources when making a new supply provider.

--- trunk/BWSAL/include/SupplyManager.h
+++ trunk/BWSAL/include/SupplyManager.h
@@ -2,13 +2,15 @@
 #include <Arbitrator.h>
 #include <BWAPI.h>
 #include <BuildManager.h>
+#include <BuildOrderManager.h>
 class SupplyManager
 {
   public:
-    SupplyManager(BuildManager* buildManager);
+    SupplyManager(BuildManager* buildManager, BuildOrderManager* buildOrderManager);
     void update();
     std::string getName() const;
     int getPlannedSupply() const;
     BuildManager* buildManager;
+    BuildOrderManager* buildOrderManager;
     int lastFrameCheck;
 };
