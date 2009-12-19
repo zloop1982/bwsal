c31680dc3cb65614c38d335189ef5a41ab6d1c0c
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2009-12-19 07:11:27 +0000

    minor update to information manager.

--- trunk/BWSAL/BasicAIModule/BasicAIModule/InformationManager.cpp
+++ trunk/BWSAL/BasicAIModule/BasicAIModule/InformationManager.cpp
@@ -7,6 +7,7 @@ void InformationManager::onUnitHide(BWAPI::Unit* unit)
   savedData[unit].player=unit->getPlayer();
   savedData[unit].type=unit->getType();
   savedData[unit].position=unit->getPosition();
+  savedData[unit].lastSeenTime=BWAPI::Broodwar->getFrameCount();
 }
 
 BWAPI::Player* InformationManager::getPlayer(BWAPI::Unit* unit)
@@ -28,6 +29,13 @@ BWAPI::Position InformationManager::getLastPosition(BWAPI::Unit* unit)
   if (unit->exists())
     return unit->getPosition();
   return savedData[unit].position;
+}
+
+int InformationManager::getLastSeenTime(BWAPI::Unit* unit)
+{
+  if (unit->exists())
+    return BWAPI::Broodwar->getFrameCount();
+  return savedData[unit].lastSeenTime;
 }
 
 InformationManager::UnitData::UnitData()
