b9d3264e8b862f241fb7fc26786441ea90d1da85
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2009-12-20 05:31:44 +0000

    Updated information manager. Now does basic inferences using required units (i.e. if it sees an enemy Mutalisk, then it knows a Spire was created at some point.

--- BWSAL/BasicAIModule/BasicAIModule/InformationManager.cpp
+++ BWSAL/BasicAIModule/BasicAIModule/InformationManager.cpp
@@ -1,6 +1,16 @@
 #include <InformationManager.h>
 InformationManager::InformationManager()
 {
+  buildTime[*BWAPI::Broodwar->enemy()->getRace().getCenter()]=0;
+  buildTime[*BWAPI::Broodwar->enemy()->getRace().getWorker()]=0;
+}
+void InformationManager::onUnitShow(BWAPI::Unit* unit)
+{
+  savedData[unit].exists=true;
+  if (!BWAPI::Broodwar->self()->isEnemy(unit->getPlayer())) return;
+  int time=BWAPI::Broodwar->getFrameCount();
+  BWAPI::UnitType type=unit->getType();
+  updateBuildTime(type,time-type.buildTime());  
 }
 void InformationManager::onUnitHide(BWAPI::Unit* unit)
 {
@@ -8,6 +18,14 @@ void InformationManager::onUnitHide(BWAPI::Unit* unit)
   savedData[unit].type=unit->getType();
   savedData[unit].position=unit->getPosition();
   savedData[unit].lastSeenTime=BWAPI::Broodwar->getFrameCount();
+}
+void InformationManager::onUnitDestroy(BWAPI::Unit* unit)
+{
+  savedData[unit].player=unit->getPlayer();
+  savedData[unit].type=unit->getType();
+  savedData[unit].position=unit->getPosition();
+  savedData[unit].lastSeenTime=BWAPI::Broodwar->getFrameCount();
+  savedData[unit].exists=false;
 }
 
 BWAPI::Player* InformationManager::getPlayer(BWAPI::Unit* unit)
@@ -36,6 +54,36 @@ int InformationManager::getLastSeenTime(BWAPI::Unit* unit)
   if (unit->exists())
     return BWAPI::Broodwar->getFrameCount();
   return savedData[unit].lastSeenTime;
+}
+
+bool InformationManager::exists(BWAPI::Unit* unit)
+{
+  if (unit->exists())
+    return true;
+  return savedData[unit].exists;
+}
+
+bool InformationManager::enemyHasBuilt(BWAPI::UnitType type)
+{
+  return (buildTime.find(type)!=buildTime.end());
+}
+int InformationManager::getBuildTime(BWAPI::UnitType type)
+{
+  std::map<BWAPI::UnitType, int>::iterator i=buildTime.find(type);
+  if (i==buildTime.end())
+    return -1;
+  return i->second;
+}
+
+void InformationManager::updateBuildTime(BWAPI::UnitType type, int time)
+{
+  std::map<BWAPI::UnitType, int>::iterator i=buildTime.find(type);
+  if (i!=buildTime.end() && i->second<=time) return;
+  buildTime[type]=time;
+  for(std::map<const BWAPI::UnitType*,int>::const_iterator i=type.requiredUnits().begin();i!=type.requiredUnits().end();i++)
+  {
+    updateBuildTime(*i->first,time-i->first->buildTime());
+  }
 }
 
 InformationManager::UnitData::UnitData()
