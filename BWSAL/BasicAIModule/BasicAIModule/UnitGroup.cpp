edc832d054dd80dda8dbc1c438a277a2dd0e4b71
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2009-12-20 02:37:16 +0000

    Added UnitGroup::getCenter and UnitGroup::withinRadius

--- BWSAL/BasicAIModule/BasicAIModule/UnitGroup.cpp
+++ BWSAL/BasicAIModule/BasicAIModule/UnitGroup.cpp
@@ -49,361 +49,398 @@ UnitGroup UnitGroup::operator-(const UnitGroup& other) const
   return result;
 }
 
-bool UnitGroup::attackMove(Position position)
+Position UnitGroup::getCenter() const
+{
+  if (this->empty())
+    return Positions::None;
+  if (this->size()==1)
+    return ((*this->begin())->getPosition());
+  int count=0;
+  double x=0;
+  double y=0;
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
+  {
+    Position p((*i)->getPosition());
+    if (p!=Positions::None && p!=Positions::Unknown)
+    {
+      count++;
+      x+=p.x();
+      y+=p.y();
+    }
+  }
+  if (count==0)
+  {
+    return Positions::None;
+  }
+  return Position((int)(x/count),(int)(y/count));
+}
+
+UnitGroup UnitGroup::withinRadius(double radius,BWAPI::Position position) const
+{
+  UnitGroup result;
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
+  {
+    if ((*i)->getDistance(position)<=radius)
+      result.insert(*i);
+  }
+  return result;
+}
+
+bool UnitGroup::attackMove(Position position) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->attackMove(position);
   }
   return retval;
 }
-bool UnitGroup::attackUnit(Unit* target)
+bool UnitGroup::attackUnit(Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->attackUnit(target);
   }
   return retval;
 }
-bool UnitGroup::rightClick(Position position)
+bool UnitGroup::rightClick(Position position) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->rightClick(position);
   }
   return retval;
 }
-bool UnitGroup::rightClick(Unit* target)
+bool UnitGroup::rightClick(Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->rightClick(target);
   }
   return retval;
 }
-bool UnitGroup::train(UnitType type)
+bool UnitGroup::train(UnitType type) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->train(type);
   }
   return retval;
 }
-bool UnitGroup::build(TilePosition position, UnitType type)
+bool UnitGroup::build(TilePosition position, UnitType type) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->build(position,type);
   }
   return retval;
 }
-bool UnitGroup::buildAddon(UnitType type)
+bool UnitGroup::buildAddon(UnitType type) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->buildAddon(type);
   }
   return retval;
 }
-bool UnitGroup::research(TechType tech)
+bool UnitGroup::research(TechType tech) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->research(tech);
   }
   return retval;
 }
-bool UnitGroup::upgrade(UpgradeType upgrade)
+bool UnitGroup::upgrade(UpgradeType upgrade) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->upgrade(upgrade);
   }
   return retval;
 }
-bool UnitGroup::stop()
+bool UnitGroup::stop() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->stop();
   }
   return retval;
 }
-bool UnitGroup::holdPosition()
+bool UnitGroup::holdPosition() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->holdPosition();
   }
   return retval;
 }
-bool UnitGroup::patrol(Position position)
+bool UnitGroup::patrol(Position position) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->patrol(position);
   }
   return retval;
 }
-bool UnitGroup::follow(Unit* target)
+bool UnitGroup::follow(Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->follow(target);
   }
   return retval;
 }
-bool UnitGroup::setRallyPosition(Position target)
+bool UnitGroup::setRallyPosition(Position target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->setRallyPosition(target);
   }
   return retval;
 }
-bool UnitGroup::setRallyUnit(Unit* target)
+bool UnitGroup::setRallyUnit(Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->setRallyUnit(target);
   }
   return retval;
 }
-bool UnitGroup::repair(Unit* target)
+bool UnitGroup::repair(Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->repair(target);
   }
   return retval;
 }
-bool UnitGroup::morph(UnitType type)
+bool UnitGroup::morph(UnitType type) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->morph(type);
   }
   return retval;
 }
-bool UnitGroup::burrow()
+bool UnitGroup::burrow() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->burrow();
   }
   return retval;
 }
-bool UnitGroup::unburrow()
+bool UnitGroup::unburrow() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->unburrow();
   }
   return retval;
 }
-bool UnitGroup::siege()
+bool UnitGroup::siege() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->siege();
   }
   return retval;
 }
-bool UnitGroup::unsiege()
+bool UnitGroup::unsiege() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->unsiege();
   }
   return retval;
 }
-bool UnitGroup::cloak()
+bool UnitGroup::cloak() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cloak();
   }
   return retval;
 }
-bool UnitGroup::decloak()
+bool UnitGroup::decloak() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->decloak();
   }
   return retval;
 }
-bool UnitGroup::lift()
+bool UnitGroup::lift() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->lift();
   }
   return retval;
 }
-bool UnitGroup::land(TilePosition position)
+bool UnitGroup::land(TilePosition position) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->land(position);
   }
   return retval;
 }
-bool UnitGroup::load(Unit* target)
+bool UnitGroup::load(Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->load(target);
   }
   return retval;
 }
-bool UnitGroup::unload(Unit* target)
+bool UnitGroup::unload(Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->unload(target);
   }
   return retval;
 }
-bool UnitGroup::unloadAll()
+bool UnitGroup::unloadAll() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->unloadAll();
   }
   return retval;
 }
-bool UnitGroup::unloadAll(Position position)
+bool UnitGroup::unloadAll(Position position) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->unloadAll(position);
   }
   return retval;
 }
-bool UnitGroup::cancelConstruction()
+bool UnitGroup::cancelConstruction() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cancelConstruction();
   }
   return retval;
 }
-bool UnitGroup::haltConstruction()
+bool UnitGroup::haltConstruction() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->haltConstruction();
   }
   return retval;
 }
-bool UnitGroup::cancelMorph()
+bool UnitGroup::cancelMorph() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cancelMorph();
   }
   return retval;
 }
-bool UnitGroup::cancelTrain()
+bool UnitGroup::cancelTrain() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cancelTrain();
   }
   return retval;
 }
-bool UnitGroup::cancelTrain(int slot)
+bool UnitGroup::cancelTrain(int slot) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cancelTrain(slot);
   }
   return retval;
 }
-bool UnitGroup::cancelAddon()
+bool UnitGroup::cancelAddon() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cancelAddon();
   }
   return retval;
 }
-bool UnitGroup::cancelResearch()
+bool UnitGroup::cancelResearch() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cancelResearch();
   }
   return retval;
 }
-bool UnitGroup::cancelUpgrade()
+bool UnitGroup::cancelUpgrade() const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->cancelUpgrade();
   }
   return retval;
 }
-bool UnitGroup::useTech(TechType tech)
+bool UnitGroup::useTech(TechType tech) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->useTech(tech);
   }
   return retval;
 }
-bool UnitGroup::useTech(TechType tech, Position position)
+bool UnitGroup::useTech(TechType tech, Position position) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->useTech(tech,position);
   }
   return retval;
 }
-bool UnitGroup::useTech(TechType tech, Unit* target)
+bool UnitGroup::useTech(TechType tech, Unit* target) const
 {
   bool retval=true;
-  for(set<Unit*>::iterator i=this->begin();i!=this->end();i++)
+  for(set<Unit*>::const_iterator i=this->begin();i!=this->end();i++)
   {
     retval = retval && (*i)->useTech(tech,target);
   }
