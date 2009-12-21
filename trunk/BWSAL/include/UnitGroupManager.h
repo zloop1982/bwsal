bab7fa34ea396f86abde720348057b51b1882e16
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2009-12-21 08:04:10 +0000

    Updated UnitGroup and UnitGroupManager. Can now do stuff like: UnitGroup idleWorkers=SelectAll()(isWorker)(isIdle);

--- trunk/BWSAL/include/UnitGroupManager.h
+++ trunk/BWSAL/include/UnitGroupManager.h
@@ -4,12 +4,15 @@
 class UnitGroupManager
 {
   public:
+    UnitGroupManager();
     void onUnitShow(BWAPI::Unit* unit);
     void onUnitHide(BWAPI::Unit* unit);
     void onUnitMorph(BWAPI::Unit* unit);
     void onUnitRenegade(BWAPI::Unit* unit);
 };
 UnitGroup AllUnits();
+UnitGroup SelectAll();
 UnitGroup SelectAll(BWAPI::UnitType type);
+UnitGroup SelectAllEnemy();
 UnitGroup SelectAllEnemy(BWAPI::UnitType type);
 UnitGroup SelectAll(BWAPI::Player* player, BWAPI::UnitType type);
