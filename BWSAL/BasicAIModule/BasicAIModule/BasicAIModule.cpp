c49869ec1fb371d56737b1554990af20385591d9
darkpyro <darkpyro@e6d74fca-808f-11de-be1a-55d290844418>
2009-12-25 01:04:01 +0000

    Moved terrain and base drawing functions into its own class. I'll be adding more elements to it soon.

--- BWSAL/BasicAIModule/BasicAIModule/BasicAIModule.cpp
+++ BWSAL/BasicAIModule/BasicAIModule/BasicAIModule.cpp
@@ -22,6 +22,7 @@ void BasicAIModule::onStart()
   this->defenseManager     = new DefenseManager(&this->arbitrator);
   this->informationManager = new InformationManager();
   this->unitGroupManager   = new UnitGroupManager();
+  this->enhancedUI         = new EnhancedUI();
 
   this->supplyManager->setBuildManager(this->buildManager);
   this->supplyManager->setBuildOrderManager(this->buildOrderManager);
@@ -125,6 +126,8 @@ void BasicAIModule::onFrame()
   this->defenseManager->update();
   this->arbitrator.update();
 
+  this->enhancedUI->update();
+
   if (Broodwar->getFrameCount()>24*50)
     scoutManager->setScoutCount(1);
 
@@ -157,59 +160,7 @@ void BasicAIModule::onFrame()
   {
     Broodwar->drawCircleMap(u->getPosition().x(),u->getPosition().y(),20,Colors::Red);
   }
-
-  //we will iterate through all the base locations, and draw their outlines.
-  for(std::set<BWTA::BaseLocation*>::const_iterator i=BWTA::getBaseLocations().begin();i!=BWTA::getBaseLocations().end();i++)
-  {
-    TilePosition p=(*i)->getTilePosition();
-    Position c=(*i)->getPosition();
-
-    //draw outline of center location
-    Broodwar->drawBox(CoordinateType::Map,p.x()*32,p.y()*32,p.x()*32+4*32,p.y()*32+3*32,Colors::Blue,false);
-
-    //draw a circle at each mineral patch
-    for(std::set<BWAPI::Unit*>::const_iterator j=(*i)->getMinerals().begin();j!=(*i)->getMinerals().end();j++)
-    {
-      Position q=(*j)->getPosition();
-      Broodwar->drawCircle(CoordinateType::Map,q.x(),q.y(),30,Colors::Cyan,false);
-    }
-
-    //draw the outlines of vespene geysers
-    for(std::set<BWAPI::Unit*>::const_iterator j=(*i)->getGeysers().begin();j!=(*i)->getGeysers().end();j++)
-    {
-      TilePosition q=(*j)->getTilePosition();
-      Broodwar->drawBox(CoordinateType::Map,q.x()*32,q.y()*32,q.x()*32+4*32,q.y()*32+2*32,Colors::Orange,false);
-    }
-
-    //if this is an island expansion, draw a yellow circle around the base location
-    if ((*i)->isIsland())
-    {
-      Broodwar->drawCircle(CoordinateType::Map,c.x(),c.y(),80,Colors::Yellow,false);
-    }
-  }
-    
-  //we will iterate through all the regions and draw the polygon outline of it in green.
-  for(std::set<BWTA::Region*>::const_iterator r=BWTA::getRegions().begin();r!=BWTA::getRegions().end();r++)
-  {
-    BWTA::Polygon p=(*r)->getPolygon();
-    for(int j=0;j<(int)p.size();j++)
-    {
-      Position point1=p[j];
-      Position point2=p[(j+1) % p.size()];
-      Broodwar->drawLine(CoordinateType::Map,point1.x(),point1.y(),point2.x(),point2.y(),Colors::Green);
-    }
-  }
-
-  //we will visualize the chokepoints with red lines
-  for(std::set<BWTA::Region*>::const_iterator r=BWTA::getRegions().begin();r!=BWTA::getRegions().end();r++)
-  {
-    for(std::set<BWTA::Chokepoint*>::const_iterator c=(*r)->getChokepoints().begin();c!=(*r)->getChokepoints().end();c++)
-    {
-      Position point1=(*c)->getSides().first;
-      Position point2=(*c)->getSides().second;
-      Broodwar->drawLine(CoordinateType::Map,point1.x(),point1.y(),point2.x(),point2.y(),Colors::Red);
-    }
-  }
+  
 }
 
 void BasicAIModule::onUnitDestroy(BWAPI::Unit* unit)
---
+++ BWSAL/include/EnhancedUI.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <BWAPI.h>
+#include <BWTA.h>
+
+class EnhancedUI
+{
+  public:
+    void update();
+
+  private:
+    void drawBases();
+    void drawTerrain();
+};
