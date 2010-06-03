0eaa34280ff71fa6448b00f93566d4d10d6a2bd9
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2010-06-03 00:39:57 +0000

    updating BWAPI header files to latest revision of bwapi

--- BWSAL/include/BWAPI/PlayerType.h
+++ BWSAL/include/BWAPI/PlayerType.h
@@ -27,22 +27,21 @@ namespace BWAPI
   {
     /** Given the name of a player type, this function will return the playertype. For example:
      *  PlayerTypes::getPlayerType("Human") will return PlayerTypes::Human. */
-    PlayerType getPlayerType(std::string& name);
+    PlayerType getPlayerType(std::string name);
 
     /** Returns the set of all the PlayerTypes. */
     std::set<PlayerType>& allPlayerTypes();
     void init();
-    extern const PlayerType NotUsed;
-    extern const PlayerType Computer;
-    extern const PlayerType Human;
-    extern const PlayerType Rescuable;
-    extern const PlayerType ComputerSlot;
-    extern const PlayerType OpenSlot;
-    extern const PlayerType Neutral;
-    extern const PlayerType ClosedSlot;
-    extern const PlayerType HumanDefeated;
-    extern const PlayerType ComputerDefeated;
     extern const PlayerType None;
+    extern const PlayerType Computer;
+    extern const PlayerType Player;
+    extern const PlayerType RescuePassive;
+    extern const PlayerType EitherPreferComputer;
+    extern const PlayerType EitherPreferHuman;
+    extern const PlayerType Neutral;
+    extern const PlayerType Closed;
+    extern const PlayerType PlayerLeft;
+    extern const PlayerType ComputerLeft;
     extern const PlayerType Unknown;
   }
 }
