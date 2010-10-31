2a486bc65d502e169b4df6d755390d77a2ac750b
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2010-10-31 21:19:50 +0000

    UnitCompositionProducer now values continuous unit production over unit composition. I.e. If it wants 2 marines : 1 medic : 0.5 firebats, it will be happy with just producing marines until it gets an academy, at which point it will make enough medics and firebats to bring the actual unit composition closer to the desired composition.

--- BWSAL/Addons/UnitCompositionProducer.cpp
+++ BWSAL/Addons/UnitCompositionProducer.cpp
@@ -5,6 +5,7 @@
 #include <TerminateIfWorkerLost.h>
 #include <TerminateIfEmpty.h>
 #include <BasicWorkerFinder.h>
+#include <MacroManager/UnitReadyTimeCalculator.h>
 using namespace BWAPI;
 using namespace std;
 UnitCompositionProducer::UnitCompositionProducer(BWAPI::UnitType workerType)
@@ -48,7 +49,7 @@ void UnitCompositionProducer::update()
   {
     if (ts->getTask()==NULL)
     {
-      UnitType t=getNextUnitType();
+      UnitType t=getNextUnitType(ts->getWorker());
       ts->setTask(Task(t));
       actualUnitCounts[t]++;
     }
@@ -57,13 +58,13 @@ void UnitCompositionProducer::update()
   {
     if (ts->getNextTask()==NULL)
     {
-      UnitType t=getNextUnitType();
+      UnitType t=getNextUnitType(ts->getWorker());
       ts->setNextTask(Task(t));
       actualUnitCounts[t]++;
     }
   }
 }
-UnitType UnitCompositionProducer::getNextUnitType()
+UnitType UnitCompositionProducer::getNextUnitType(BWAPI::Unit* worker)
 {
   double aTc = 0;
   double cTc = 0;
@@ -77,11 +78,15 @@ UnitType UnitCompositionProducer::getNextUnitType()
   {
     desiredUnitCounts[t.first]=t.second*r;
   }
-  double maxDiff=-1;
+  double maxDiff=-10000;
   UnitType nextType;
+  int time = UnitReadyTimeCalculator::getReadyTime(worker,true);
   for each(std::pair<UnitType, double> t in unitCompositionWeights)
   {
     double d=desiredUnitCounts[t.first]-actualUnitCounts[t.first];
+    UnitReadyTimeStatus::Enum r;
+    int t2=UnitReadyTimeCalculator::getReadyTime(worker,Task(t.first),r,false,true);
+    if (t2<0 || t2>time) continue;
     if (d>maxDiff)
     {
       maxDiff=d;
