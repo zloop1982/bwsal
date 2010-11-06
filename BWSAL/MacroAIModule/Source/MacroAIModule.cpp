6612d6056b8ae988660c37de1ea40104b84888fd
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2010-11-06 03:33:35 +0000

    A few initial changes to start working on protoss support for the macro manager, however I have yet to implement the main changes that will be needed:
    I think the main change will need to be a way to have a task run and complete with no worker specified (since a building task should be able to release a worker once the building starts warping in). Also I'll need to add a way to fork off tasks when multiple building tasks are in a task stream so that a probe can immediately start the next building once the first building in the task stream is done. Training units and researching upgrades and abilities probably won't need any changes.

--- BWSAL/MacroAIModule/Source/MacroAIModule.cpp
+++ BWSAL/MacroAIModule/Source/MacroAIModule.cpp
@@ -48,20 +48,31 @@ void MacroAIModule::onStart()
   Unit* worker = NULL;
   for each(Unit* u in Broodwar->self()->getUnits())
   {
-    if (u->getType()==UnitTypes::Terran_Command_Center)
+    if (u->getType().isResourceDepot())
       worker = u;
   }
   ts->setWorker(worker);
   ts->attach(BasicTaskExecutor::getInstance(),false);
-  ts->attach(new UnitPump(UnitTypes::Terran_SCV),true);
+  ts->attach(new UnitPump(Broodwar->self()->getRace().getWorker()),true);
   ts->attach(new TerminateIfWorkerLost(),true);
-  infantryProducer = new UnitCompositionProducer(UnitTypes::Terran_Barracks);
-  infantryProducer->setUnitWeight(UnitTypes::Terran_Marine,2.0);
-  infantryProducer->setUnitWeight(UnitTypes::Terran_Medic,1.0);
-  infantryProducer->setUnitWeight(UnitTypes::Terran_Firebat,0.5);
-  vehicleProducer = new UnitCompositionProducer(UnitTypes::Terran_Factory);
-  vehicleProducer->setUnitWeight(UnitTypes::Terran_Vulture,2.0);
-  vehicleProducer->setUnitWeight(UnitTypes::Terran_Siege_Tank_Tank_Mode,1.0);
+
+  if (Broodwar->self()->getRace()==Races::Terran)
+  {
+    infantryProducer = new UnitCompositionProducer(UnitTypes::Terran_Barracks);
+    infantryProducer->setUnitWeight(UnitTypes::Terran_Marine,2.0);
+    infantryProducer->setUnitWeight(UnitTypes::Terran_Medic,1.0);
+    infantryProducer->setUnitWeight(UnitTypes::Terran_Firebat,0.5);
+
+    vehicleProducer = new UnitCompositionProducer(UnitTypes::Terran_Factory);
+    vehicleProducer->setUnitWeight(UnitTypes::Terran_Vulture,2.0);
+    vehicleProducer->setUnitWeight(UnitTypes::Terran_Siege_Tank_Tank_Mode,1.0);
+  }
+  if (Broodwar->self()->getRace()==Races::Protoss)
+  {
+    infantryProducer = new UnitCompositionProducer(UnitTypes::Protoss_Gateway);
+    infantryProducer->setUnitWeight(UnitTypes::Protoss_Dragoon,2.0);
+    infantryProducer->setUnitWeight(UnitTypes::Protoss_Zealot,1.0);
+  }
 }
 void MacroAIModule::onEnd(bool isWinner)
 {
@@ -69,8 +80,10 @@ void MacroAIModule::onEnd(bool isWinner)
 void MacroAIModule::onFrame()
 {
   TheArbitrator->update();
-  infantryProducer->update();
-  vehicleProducer->update();
+  if (infantryProducer)
+    infantryProducer->update();
+  if (vehicleProducer)
+    vehicleProducer->update();
   TheMacroSupplyManager->update();
   TheMacroDependencyResolver->update();
   TheMacroManager->update();
