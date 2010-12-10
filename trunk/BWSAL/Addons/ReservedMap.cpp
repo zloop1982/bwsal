36a156401b8b980ae3fb2c46afe77d99756b4228
lowerlogic <lowerlogic@e6d74fca-808f-11de-be1a-55d290844418>
2010-12-10 10:25:27 +0000

    Fixed two bugs with the BFSBuildingPlacer and added a PylonBuildingPlacer which makes sure to space pylons far apart from each other (though they can be closer to other types of buildings).

--- trunk/BWSAL/Addons/ReservedMap.cpp
+++ trunk/BWSAL/Addons/ReservedMap.cpp
@@ -15,29 +15,37 @@ ReservedMap::ReservedMap()
 {
   TheReservedMap = this;
   reserveMap.resize(Broodwar->mapWidth(),Broodwar->mapHeight());
-  reserveMap.setTo(false);
+  reserveMap.setTo(UnitTypes::None);
 }
 ReservedMap::~ReservedMap()
 {
   TheReservedMap = NULL;
 }
-void ReservedMap::reserveTiles(BWAPI::TilePosition position, int width, int height)
+void ReservedMap::reserveTiles(BWAPI::TilePosition position, BWAPI::UnitType type, int width, int height)
 {
   for(int x = position.x(); x < position.x() + width && x < (int)reserveMap.getWidth(); x++)
     for(int y = position.y(); y < position.y() + height && y < (int)reserveMap.getHeight(); y++)
-      reserveMap[x][y] = true;
+      reserveMap[x][y] = type;
 }
 void ReservedMap::freeTiles(BWAPI::TilePosition position, int width, int height)
 {
   for(int x = position.x(); x < position.x() + width && x < (int)reserveMap.getWidth(); x++)
     for(int y = position.y(); y < position.y() + height && y < (int)reserveMap.getHeight(); y++)
-      reserveMap[x][y] = false;
+      reserveMap[x][y] = UnitTypes::None;
 }
 bool ReservedMap::isReserved(int x, int y)
 {
-  return reserveMap[x][y];
+  return reserveMap[x][y] != UnitTypes::None;
 }
 bool ReservedMap::isReserved(TilePosition p)
+{
+  return reserveMap[p.x()][p.y()] != UnitTypes::None;
+}
+BWAPI::UnitType ReservedMap::getReservedType(int x, int y)
+{
+  return reserveMap[x][y];
+}
+BWAPI::UnitType ReservedMap::getReservedType(BWAPI::TilePosition p)
 {
   return reserveMap[p.x()][p.y()];
 }
--- trunk/BWSAL/include/ReservedMap.h
+++ trunk/BWSAL/include/ReservedMap.h
@@ -6,13 +6,15 @@ class ReservedMap
   public:
     static ReservedMap* create();
     static void destroy();
-    void reserveTiles(BWAPI::TilePosition position, int width = 1, int height = 1);
+    void reserveTiles(BWAPI::TilePosition position, BWAPI::UnitType type, int width = 1, int height = 1);
     void freeTiles(BWAPI::TilePosition position, int width = 1, int height = 1);
     bool isReserved(int x, int y);
     bool isReserved(BWAPI::TilePosition p);
+    BWAPI::UnitType getReservedType(int x, int y);
+    BWAPI::UnitType getReservedType(BWAPI::TilePosition p);
   private:
     ReservedMap();
     ~ReservedMap();
-    Util::RectangleArray<bool> reserveMap;
+    Util::RectangleArray<BWAPI::UnitType> reserveMap;
 };
 extern ReservedMap* TheReservedMap;
